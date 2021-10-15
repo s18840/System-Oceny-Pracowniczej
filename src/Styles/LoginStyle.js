118f25b8a6aa1047d86ddb1f4c7e8440e7f25630
jsz <jsz@novelpay.pl>
2021-10-15 11:26:41 +0200

    basic login component

---
+++ src/Styles/LoginStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LoginWrapper= styled.div`
+    width: 457px;
+    height: 700px;
+    border: 5px solid white;
+    border-radius: 48px;
+    background-color: #FFF3FD
+`;
