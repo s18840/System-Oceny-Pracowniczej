8de0d9d1037f9ca1a863dbc2787b456058a1a89a
s18636 <s18636@pjwstk.edu.pl>
2021-10-15 13:15:20 +0200

    Project structure and Global font

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,13 @@
+import {createGlobalStyle} from 'styled-components/macro';
+
+export const GlobalStyle = createGlobalStyle`
+  body{
+    margin: 0;
+    padding: 0;
+    color: black;
+    font-size: 1.2em;
+    font-family: ubuntu, sans-serif;
+    font-style: normal;
+    font-weight: 300;
+  }
+`;
