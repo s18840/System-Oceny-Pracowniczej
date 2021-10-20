1f51e16d65a9344713a6a0c483734b13334df29a
s18636 <s18636@pjwstk.edu.pl>
2021-10-20 08:27:37 +0200

    Nav and main screen layout work in progress

---
+++ src/Styles/WelcomePageStyle.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const WelcomeTopWrapper = styled.div`
+    display: flex;
+    float: right;
+    margin-right: 150px;
+    justify-content: space-between;
+
+`
+
+export const WelcomeNavButton = styled.a`
+    display: block;
+    width: 248px;
+    height: 83px;
+    color: #FF4E01;
+    background-color: #FFF3FD;
+    text-align: center;
+    border: 3px solid #FEFFFF;
+    border-radius: 40px;
+    font-size: 39;
+    margin: 23px;
+`
+
+export const WelcomeNav = styled.li`
+    display: flex;
+    text-align:center; 
+`
---
+++ src/components/WelcomeNav.js
@@ -0,0 +1,16 @@
+import react from "react";
+import { WelcomeNavButton, WelcomeNav } from "../styles/WelcomePageStyle";
+
+
+function Nav(){
+    return (
+        <WelcomeNav>
+            <WelcomeNavButton>Home</WelcomeNavButton>
+            <WelcomeNavButton>Home</WelcomeNavButton>
+            <WelcomeNavButton>Home</WelcomeNavButton>
+        </WelcomeNav>
+
+    );
+}
+
+export default Nav;
