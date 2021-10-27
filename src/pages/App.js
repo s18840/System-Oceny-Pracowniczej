e22ec71302121383ee0d312628d042f89163d94c
s18636 <s18636@pjwstk.edu.pl>
2021-10-27 20:04:26 +0200

    merge fix

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {GlobalStyle} from '../styles/GlobalStyle'
+import WelcomePage from './WelcomePage'
+
+function App() {
+  return (
+  <>
+    <GlobalStyle/>
+    <WelcomePage/>
+  </>
+  );
+}
+
+export default App;
