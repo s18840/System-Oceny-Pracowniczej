1eed809b758f3b834ad7c435c90ce96377ccfc64
s18636 <s18636@pjwstk.edu.pl>
2021-11-04 20:19:02 +0100

    forms on submit send data in json. hashing and checking hashed password

--- src/components/WelcomePage/ContactForm.js
+++ src/components/WelcomePage/ContactForm.js
@@ -9,7 +9,7 @@ function ContactForm() {
   const { t } = useTranslation()
 
   //TODO add errors for form validation
-  const { register, handleSubmit, watch, errors } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const submitForm = (data) => {
     console.log(data);
   }
@@ -18,16 +18,16 @@ function ContactForm() {
     <ContactFormWrapper onSubmit={handleSubmit(submitForm)}>
       <Span fontSize="30px">{t('NAME')}</Span>
       <InputField
-        {...register("name")}
-        name="name"
+        {...register("name", ({required: true}))}
         height="50px"
       />
+      {errors.name && errors.name.type === "required" && <Span>{t('REQUIRED')}</Span>}
       <Span fontSize="30px">{t('MSG')}</Span>
       <TextField
-        {...register("message")}
-        name="message"
+        {...register("message", ({required: true}))}
         height="470px"
       />
+      {errors.message && errors.message.type === "required" && <Span>{t('REQUIRED')}</Span>}
       <OrangeInputButton type="submit" value={t('SEND')}/>
     </ContactFormWrapper>
   )
--- src/components/WelcomePage/LoginForm.js
+++ src/components/WelcomePage/LoginForm.js
@@ -1,65 +1,48 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
-import { useForm } from "react-hook-form";
-import sha256 from 'crypto-js/sha256'
+import {useTranslation} from 'react-i18next'
+import {useForm} from "react-hook-form";
+import {InputWrapper, Login, LoginButton, LoginFormWrapper,} from '../../styles/LoginStyle'
+import {InputField, Span} from '../../styles/GlobalStyle'
+import {hashPassword, isPasswordCorrect} from '../../Utils/PasswordUtils'
 
-import {
-  LoginFormWrapper,
-  InputWrapper,
-  Login,
-  LoginButton,
-} from '../../styles/LoginStyle'
-import { InputField, Span } from '../../styles/GlobalStyle'
 
 //TODO add errors for form validation
 function LoginForm() {
-  const { t } = useTranslation()
-  var log = require('loglevel')
-  const { register, handleSubmit} = useForm();
+  const {t} = useTranslation()
+  const {register, handleSubmit, formState: {errors}} = useForm();
   const submitForm = (data) => {
-    console.log(data);
-
-    //TODO encrypt password
-
-    // const hashedPassword = bcrypt.hashSync(data.password, salt);
-
-    // let credentials = {
-    //   "username": data.username,
-    //   "password": hashedPassword
-    // };
-
-    // console.log(credentials);/
+    let hashedPassword = hashPassword(data.password)
   };
 
 
   return (
-      <LoginFormWrapper onSubmit={handleSubmit(submitForm)}>
-        <Login>{t('LOGIN')}</Login>
-        <InputWrapper>
-          <Span fontSize="20px">{t('USERNAME')}</Span>
-          <InputField
-            {...register("username", ({required: true}))}
-            width="357px"
-            height="50px"
-            name="username"
-            type="text"
-          />
+    <LoginFormWrapper onSubmit={handleSubmit(submitForm)}>
+      <Login>{t('LOGIN')}</Login>
+      <InputWrapper>
+        <Span fontSize="20px">{t('USERNAME')}</Span>
+        <InputField
+          {...register("username", ({required: true}))}
+          width="357px"
+          height="50px"
+          type="text"
+        />
+        {errors.username && errors.username.type === "required" && <Span>{t('REQUIRED')}</Span>}
 
-          <Span fontSize="20px">{t('PASSWD')}</Span>
-          <InputField
-            {...register("password", ({required: true}))}
-            width="357px"
-            height="50px"
-            name="password"
-            type="password"
-          />
-        </InputWrapper>
+        <Span fontSize="20px">{t('PASSWD')}</Span>
+        <InputField
+          {...register("password", ({required: true}))}
+          width="357px"
+          height="50px"
+          type="password"
+        />
+        {errors.password && errors.password.type === "required" && <Span>{t('REQUIRED')}</Span>}
+      </InputWrapper>
 
-        <LoginButton type="submit" value="login"/>
-        <Span fontSize="20px" underlined>
-          {t('FORGOT_PASSWD')}
-        </Span>
-      </LoginFormWrapper>
+      <LoginButton type="submit" value="login"/>
+      <Span fontSize="20px" underlined>
+        {t('FORGOT_PASSWD')}
+      </Span>
+    </LoginFormWrapper>
   )
 }
 
