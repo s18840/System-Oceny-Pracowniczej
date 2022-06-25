2945365d2ea9bff582924ba4f567ff45ad9109dc
s18840 <Mikoaj@DESKTOP-G6E9EB5>
2022-06-25 13:25:03 +0200

    Connected education tab with API

--- src/components/ProfilePage/EducationInformation.js
+++ src/components/ProfilePage/EducationInformation.js
@@ -1,49 +1,49 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import axios from 'axios';
 import {DateWrapper, EmploymentTableDateLine, Row, TableDetailsDate, TableInfo,} from "../../styles/ProfilePageStyle";
 import { useTranslation } from "react-i18next";
 import useApi from "../../api/useApi";
+import { Context } from '../../pages/Context';
 //const today = new Date();
 
-const dataJson = {
-  titles: ["Time", "Institution", "Degree"],
-  content: [
-    {
-      StartDate: "14.05.2013",
-      Institution: "Politechnika Warszawska",
-      Degree: "Geography",
-    },
-    {
-      StartDate: "14.05.2016",
-      Institution: "PJATK",
-      Degree: "Programming",
-    },
-    {
-      StartDate: "08.09.2010",
-      Institution: "Liceum XVI im. A.Mickiewicza",
-      Degree: "-",
-    },
-  ],
-};
+const dataJson = ["Graduation Date", "Institution", "Degree",];
 function EducationInformation () {
-  let empId =7;
-  const { loading , emp } = useApi(`https://localhost:5001/api/Education/emp/${empId}`);
-  console.log(emp)
+  const [context, setContext] = useContext(Context);
+  const [employee, setEmployee] = useState();
+  const [graduationDate, setGraduationDate] = useState(" ");
+  const [degree, setDegree] = useState(" ");
+  const [placeOfEducation, setPlaceOfEducation] = useState(" ");
   const { t } = useTranslation();
+  useEffect (()=>{
+    context && axios.get(`https://localhost:5001/api/Dto/emp/${context.employeeId}`, {headers: {Authorization: `Bearer ${context.token}` }}).then(({data}) => 
+    {setEmployee(data); 
+      setPlaceOfEducation(data.educations.placeOfEducation);
+      setDegree(data.educations.degree);
+      setGraduationDate(data.educations.graduationDate);
+    });
+  },[context]);
+
+  let dArr;
+  function reformatDate(dateStr){
+    dArr = dateStr.split("-");
+    return dArr[2]+ "-" +dArr[1]+ "-" +dArr[0];
+  }
+
   return(
   <>
     <TableInfo className="table">
       <thead>
       <tr>
-        {dataJson.titles.map((title) => (
+        {dataJson.map((title) => (
           <th>{t(title)}</th>
         ))}
       </tr>
       </thead>
-      {dataJson.content.map((content) => (
+      {employee?.educations?.map((content) => (
         <Row>
-          <td>{content.StartDate}</td>
-          <td>{content.Institution}</td>
-          <td>{content.Degree}</td>
+          <td>{reformatDate(content?.graduationDate.split('T')[0] ? content?.graduationDate.split('T')[0] :'')}</td>
+          <td>{content.placeOfEducation ? content.placeOfEducation : "-"}</td>
+          <td>{content.degree}</td>
         </Row>
       ))}
     </TableInfo>
