ac8364d16b877c434bb9eb427c49c014d410af0f
s18840 <Mikoaj@DESKTOP-G6E9EB5>
2022-07-24 18:37:29 +0200

    Team list and details

---
+++ src/pages/TeamDetails.js
@@ -0,0 +1,131 @@
+import React, { useEffect, useState, useContext } from "react";
+import Header from "../components/Header/Header";
+import NavBar from "../components/Navigation/NavBar";
+import Footer from "../components/Footer/Footer";
+import ProfileInfo from "../components/ProfilePage/ProfileInfoDiv";
+import { Context } from "../pages/Context";
+import axios from "axios";
+import {
+    ProfileHeaderText,
+    ProfileInfoDiv,
+    ProfileSubHeaderText,
+    ProfileText,
+    ProfileTextWrapper,
+    StatusIcon,
+    ProfileAvatar,
+
+  } from "../styles/ProfilePageStyle";
+import { 
+  PageWrapper,
+  TeamsWrapper,
+  TableTeams,
+  RowLi,
+  Heading,
+  ProfileDataText,
+} from "../styles/GlobalStyle";
+
+function TeamDetails(props) {
+    const [context] = useContext(Context);
+    const [employee, setEmployee] = useState([]);
+    const [competences, setCompetences] = useState([]);
+    const [status, setStatus] = useState(" ");
+
+    useEffect(() => {
+        context &&
+          axios
+            .get(
+              `https://localhost:5001/api/Employee/team/${localStorage.getItem(
+                "employeeId"
+              )}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+              }
+            )
+            .then(({ data }) => {
+              console.log(data)
+              setEmployee(data);
+              setStatus(data.status);
+            });
+      }, [context]);
+    useEffect(() => {
+      context &&
+        axios
+          .get(
+            `https://localhost:5001/api/Dto/comps/${localStorage.getItem(
+              "employeeId"
+            )}`,
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+              },
+            }
+          )
+          .then(({ data }) => {
+            console.log(data)
+            setCompetences(data);
+          });
+    }, [context]);
+    console.log(competences)
+  return (
+    <>
+      <NavBar/>
+      <Header/>
+      <Footer/>
+      <PageWrapper style={{marginBottom:40}}>
+        <Heading style={{marginLeft:30, marginTop:30}}>
+          <ProfileDataText>Members of this team: </ProfileDataText>
+        </Heading>
+        {employee?.map((emp) => (
+        <ProfileInfoDiv style={{marginTop:20}}>
+          <ProfileAvatar to="/profile">
+            {(emp.firstName[0]) + (emp.lastName[0])}
+          </ProfileAvatar>
+          <ProfileTextWrapper>
+            <ProfileHeaderText>
+              {emp.firstName + " " + emp.lastName}
+            </ProfileHeaderText>
+          </ProfileTextWrapper>
+          <ProfileTextWrapper>
+          <ProfileSubHeaderText>
+              {"Status" + ": "}
+              <StatusIcon
+                style={
+                  status > 0
+                    ? { backgroundColor: "#55ff11" }
+                    : { backgroundColor: "#ff5511" }
+                }
+              />
+            </ProfileSubHeaderText>
+            <ProfileSubHeaderText>
+              {"Personal number" + ": "}
+              <ProfileText>{" " + emp.personalNumber}</ProfileText>
+            </ProfileSubHeaderText>
+          </ProfileTextWrapper>
+        </ProfileInfoDiv>
+        ))}
+        <Heading style={{marginLeft:30, marginTop:30}}>
+          <ProfileDataText>Competences in this team: </ProfileDataText>
+        </Heading>
+        <TeamsWrapper style={{marginLeft:30}}>
+          <TableTeams className="table">
+            {competences?.map((el) => (
+              <tr>
+                <td>
+                  <RowLi>
+                    {el.name}
+                  </RowLi>
+                </td>
+              </tr>
+            ))}
+          </TableTeams>
+        </TeamsWrapper>
+      </PageWrapper>
+
+    </>
+
+  );
+}
+
+export default TeamDetails;
