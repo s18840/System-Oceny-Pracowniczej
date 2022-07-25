a6fe9ae7241f936aabda3dac00d1072b439a49f7
Mikołaj Sroczyński <61829613+s18840@users.noreply.github.com>
2022-07-25 23:29:40 +0200

    Adding styles

---
+++ src/styles/FormEmpStyles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const EmployeeAddWrapper = styled.div`
+  padding-bottom: 5px;
+  padding-left: 15px;
+  border: 2px solid #dfdfdf;
+  margin: 10px 10px 10px 10px;
+  border-radius: 20px;
+  width: 900px;
+  display: flex;
+  flex-direction: column;
+  color: blue;
+`;
